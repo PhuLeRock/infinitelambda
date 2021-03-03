e48ff5ebbb7bcdc8c12a6cd0704d581eda82193a
phulerock <phulerock@gmail.com>
2021-03-03 23:28:37 +0700

    pythonapp

--- pythonapp/pyapp.py
+++ pythonapp/pyapp.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python
-import os
 import psycopg2
 import subprocess
 
+
 conn_str = {
-        'user': subprocess.check_output('aws ssm get-parameters --names /rds/db/rds-identifier/superuser/username --query "Parameters[*].Value"', shell=True).strip(),
-        'host': subprocess.check_output('aws ssm get-parameters --names /rds/db/rds-identifier/endpoint --query "Parameters[*].Value" | cut -d : -f 1', shell=True).strip(),
+        'user': subprocess.check_output('aws ssm get-parameters --names /rds/db/rds-identifier/superuser/username --query "Parameters[*].Value"', shell=True).decode().strip(),
+        'host': subprocess.check_output('aws ssm get-parameters --names /rds/db/rds-identifier/endpoint --query "Parameters[*].Value" | cut -d : -f 1', shell=True).decode().strip(),
         'port': 5432,
         'database': 'postgres',
-        'password': subprocess.check_output('aws ssm get-parameters --names /rds/db/rds-identifier/superuser/password --query "Parameters[*].Value"', shell=True).strip()
+        'password': subprocess.check_output('aws ssm get-parameters --names /rds/db/rds-identifier/superuser/password --query "Parameters[*].Value"', shell=True).decode().strip()
     }
 conn = psycopg2.connect(**conn_str)
 
